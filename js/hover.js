342b879d78fb5ba77ffbf5a1022edaeb9d461551
Szymon Sobik <szymon.sobik13@gmail.com>
2016-11-02 12:34:26 +0100

    Poprawa płynności animacji

--- js/hover.js
+++ js/hover.js
@@ -3,10 +3,12 @@ function hover(n)
 {
 	$("#tile"+n).addClass("hover"+n)
 	$("body").addClass("tlo"+n);
+	$("body").removeClass("tlo");
 }
 
 function out(n)
 {
 	$("#tile"+n).removeClass("hover"+n)
 	$("body").removeClass("tlo"+n);
+	$("body").addClass("tlo");
 }
